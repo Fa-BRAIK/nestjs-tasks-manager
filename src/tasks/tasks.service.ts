c3ed2286e6d291c66e82938adb0b720ff63ff764
Farouk BRAIK <faroukbraik17@gmail.com>
2020-08-09 15:16:07 -0400

    getting tasks with filters

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,10 +2,35 @@ import { Injectable } from '@nestjs/common'
 import { ITask, TaskStatus } from './task.model'
 import { CreateTaskDto } from './dto/create-task.dto'
 import * as uuid from 'uuid'
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto'
 
 @Injectable()
 export class TasksService {
   private _tasks: ITask[] = []
+
+  tasks(): ITask[] {
+    return this._tasks
+  }
+
+  tasksFilter(filterDto: GetTasksFilterDto): ITask[] {
+    const { status, search } = filterDto
+
+    let tasks = this.tasks()
+
+    if (status) tasks = tasks.filter(task => task.status === status)
+
+    if (search)
+      tasks = tasks.filter(
+        task =>
+          task.title.includes(search) || task.description.includes(search),
+      )
+
+    return tasks
+  }
+
+  task(id: string): ITask {
+    return this._tasks.find(task => task.id === id)
+  }
 
   create(createTaskDto: CreateTaskDto): ITask {
     const { title, description } = createTaskDto
@@ -29,13 +54,5 @@ export class TasksService {
 
   delete(id: string): void {
     this._tasks.filter(task => task.id !== id)
-  }
-
-  task(id: string): ITask {
-    return this._tasks.find(task => task.id === id)
-  }
-
-  get tasks(): ITask[] {
-    return this._tasks
   }
 }
