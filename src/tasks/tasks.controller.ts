6c5b9c1622cd9921fdd979b1b393469afa27df10
Farouk BRAIK <faroukbraik17@gmail.com>
2020-08-17 21:14:40 -0400

    updating controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -9,13 +9,20 @@ import {
   Query,
   UsePipes,
   ValidationPipe,
+  ParseIntPipe,
 } from '@nestjs/common'
 import { TasksService } from './tasks.service'
 import { CreateTaskDto } from './dto/create-task.dto'
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto'
 import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe'
+import { Task } from './task.entity'
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
+
+  @Get(':id')
+  async findOne(@Param('id', ParseIntPipe) id: number): Promise<Task> {
+    return this.tasksService.findOne(id)
+  }
 }
