29b3183806cbb43cdfa5371094356617b77b4fea
Farouk BRAIK <faroukbraik17@gmail.com>
2020-08-21 23:38:32 -0400

    adding a testing route of auth guards

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,14 @@
-import { Controller, Post, Body, ValidationPipe } from '@nestjs/common'
+import {
+  Controller,
+  Post,
+  Body,
+  ValidationPipe,
+  UseGuards,
+  Req,
+} from '@nestjs/common'
 import { AuthCredentialsDto } from './dto/auth-credentials.dto'
 import { AuthService } from './auth.service'
+import { AuthGuard } from '@nestjs/passport'
 
 @Controller('auth')
 export class AuthController {
@@ -19,4 +27,8 @@ export class AuthController {
   ): Promise<{ access_token: string }> {
     return await this.authService.signIn(authCredentialsDto)
   }
+
+  @Post('test')
+  @UseGuards(AuthGuard())
+  public async test(@Req() req) {}
 }
